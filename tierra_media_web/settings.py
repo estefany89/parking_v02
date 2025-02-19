03254fbd2a52bb76ee14d6fb20e3e716a483db4d
JesusJimenez01 <jjimper591@g.educaand.es>
2025-02-19 18:41:24 +0100

    Migrar a postgres

--- tierra_media_web/settings.py
+++ tierra_media_web/settings.py
@@ -83,11 +83,16 @@ WSGI_APPLICATION = 'tierra_media_web.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'django_db',
+       'USER': 'postgres',
+       'PASSWORD': 'password',
+       'HOST': 'db',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation
